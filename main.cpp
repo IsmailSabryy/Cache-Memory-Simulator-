515e5cc159923846c2b471d9cbad3ba1f3d1b23b
Abouheiba <abouheiba@aucegypt.edu>
2024-05-19 13:59:27 +0300

    Implemented the GUI accordingly.

--- main.cpp
+++ main.cpp
@@ -9,99 +9,7 @@ using namespace std;
 vector<pair<bool, string>> cache;
 vector<pair<bool, string>> iCache;
 int memoryclock = 100;
-char menu[4][45]={
-        {" 1]      Test   "},
-        {" 2]      Test  "},
-        {" 3]      Test          "},
-        {" 4]               Exit      "}
-};
-void ShowConsoleCursor(bool showFlag)
-{
-    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    CONSOLE_CURSOR_INFO     cursorInfo;
-
-    GetConsoleCursorInfo(out, &cursorInfo);
-    cursorInfo.bVisible = showFlag; // set the cursor visibility
-    SetConsoleCursorInfo(out, &cursorInfo);
-}
-// function to locate cursor at row and col in screen
-void goto_row_col(int row,int col)
-{
-    COORD c;
-    c.X = col;
-    c.Y = row;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-}
-
-void setColor(int color)
-{
-    HANDLE hConsole;
-
-    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, color);
-}
-
-void showMenu()
-{
-    char ch;
-    int pos=1;
-
-    do
-    {
-        ShowConsoleCursor(false);
-        setColor(15);
-        goto_row_col(10,39);cout << menu[0];
-        goto_row_col(11,39);cout << menu[1];
-        goto_row_col(12,39);cout << menu[2];
-        goto_row_col(13,39);cout << menu[3];
-        setColor(240);
-        goto_row_col(10+pos-1,39);cout << menu[pos-1];
-        ch = getch();
-        switch (ch)
-        {
-            case 13:
-                switch(pos)
-                {
-                    case 1:
-                        setColor(15);
-                        ShowConsoleCursor(true);
-                        system("cls");
-
-                        break;
-                    case 2:
-                        setColor(15);
-                        ShowConsoleCursor(true);
-                        system("cls");
-
-                        break;
-                    case 3:
-                        setColor(15);
-                        ShowConsoleCursor(true);
-
-                    case 4:
-                        setColor(15);
-                        ShowConsoleCursor(true);
-                        exit(0);
-                        break;
-                }
-                break;
-            case -32:
-                ch = getch();
-                switch (ch)
-                {
-                    case 72:
-                        pos--;
-                        if (pos ==0) pos =4;
-                        break;
-                    case 80:
-                        pos++;
-                        if (pos ==5) pos =1;
-                        break;
-                }
-        }
-    } while(true);
-}
 string strbin_to_dec(string binary)
 {
     int base = 1;
@@ -256,62 +164,147 @@ void Icachesimulatorline(string address, int cycles, int index, int disp, int ta
     cout << "Miss ratio: " << missrate << endl;
     cout << "AMAT: " << double(cycles) + double(memoryclock * missrate) << "ns" << endl;
 }
+
+char menu[4][45]={
+        {" 1]      Start   "},
+        {" 2]      Exit    "},
+};
+void ShowConsoleCursor(bool showFlag)
+{
+    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    CONSOLE_CURSOR_INFO     cursorInfo;
+
+    GetConsoleCursorInfo(out, &cursorInfo);
+    cursorInfo.bVisible = showFlag; // set the cursor visibility
+    SetConsoleCursorInfo(out, &cursorInfo);
+}
+// function to locate cursor at row and col in screen
+void goto_row_col(int row,int col)
+{
+    COORD c;
+    c.X = col;
+    c.Y = row;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+
+void setColor(int color)
+{
+    HANDLE hConsole;
+
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, color);
+}
+void option1()
+{
+string filename;
+
+int S, L, Cycles, C, index, disp, tag, numofaccess, hits, misses;
+cout << "Enter the total cache size" << endl;
+cin >> S;
+cout << "Enter the cache line size" << endl;
+cin >> L;
+cout << "Enter the number of cycles needed to access the cache " << endl;
+cin >> Cycles;
+cout << "Enter filepath: " << endl;
+cin >> filename;
+C = S / L;
+index = log2(C);
+disp = log2(L);
+tag = 32 - index - disp;
+cache.resize(C); // ?
+iCache.resize(C);
+for (int i = 0; i < C; i++)
+{
+cache[i].first = false;
+for (int j = 0; j < tag; j++)
+{
+cache[i].second += '0';
+}
+}
+for (int i = 0; i < C; i++)
+{
+iCache[i].first = false;
+for (int j = 0; j < tag; j++)
+{
+iCache[i].second += '0';
+}
+}
+numofaccess = 0;
+hits = 0;
+misses = 0;
+ifstream filereader(filename);
+string fileline, address;
+while (getline(filereader, fileline))
+{
+if (fileline[0] == 'd' || fileline[0] == 'D')
+{
+address = fileline.substr(1);
+cachesimulatorline(address, Cycles, index, disp, tag, numofaccess, hits, misses);
+}
+else if (fileline[0] == 'i' || fileline[0] == 'I')
+{
+address = fileline.substr(1);
+Icachesimulatorline(address, Cycles, index, disp, tag, numofaccess, hits, misses);
+}
+// else
+// {
+//     address = fileline;
+//     cachesimulatorline(address, Cycles, index, disp, tag, numofaccess, hits, misses);
+// }
+}
+}
+void showMenu()
+{
+    char ch;
+    int pos=1;
+
+    do
+    {
+        ShowConsoleCursor(false);
+        setColor(15);
+        goto_row_col(10,39);cout << menu[0];
+        goto_row_col(11,39);cout << menu[1];
+        goto_row_col(12,39);cout << menu[2];
+        goto_row_col(13,39);cout << menu[3];
+        setColor(240);
+        goto_row_col(10+pos-1,39);cout << menu[pos-1];
+        ch = getch();
+        switch (ch)
+        {
+            case 13:
+                switch(pos)
+                {
+                    case 1:
+                        setColor(15);
+                        ShowConsoleCursor(true);
+                        system("cls");
+                        option1();
+                        break;
+                    case 2:
+                        setColor(15);
+                        ShowConsoleCursor(true);
+                        exit(0);
+                        break;
+                }
+                break;
+            case -32:
+                ch = getch();
+                switch (ch)
+                {
+                    case 72:
+                        pos--;
+                        if (pos ==0) pos =4;
+                        break;
+                    case 80:
+                        pos++;
+                        if (pos ==5) pos =1;
+                        break;
+                }
+        }
+    } while(true);
+}
 int main()
 {
-    string filename;
-
-    int S, L, Cycles, C, index, disp, tag, numofaccess, hits, misses;
-    cout << "Enter the total cache size" << endl;
-    cin >> S;
-    cout << "Enter the cache line size" << endl;
-    cin >> L;
-    cout << "Enter the number of cycles needed to access the cache " << endl;
-    cin >> Cycles;
-    cout << "Enter filepath: " << endl;
-    cin >> filename;
-    C = S / L;
-    index = log2(C);
-    disp = log2(L);
-    tag = 32 - index - disp;
-    cache.resize(C); // ?
-    iCache.resize(C);
-    for (int i = 0; i < C; i++)
-    {
-        cache[i].first = false;
-        for (int j = 0; j < tag; j++)
-        {
-            cache[i].second += '0';
-        }
-    }
-    for (int i = 0; i < C; i++)
-    {
-        iCache[i].first = false;
-        for (int j = 0; j < tag; j++)
-        {
-            iCache[i].second += '0';
-        }
-    }
-    numofaccess = 0;
-    hits = 0;
-    misses = 0;
-    ifstream filereader(filename);
-    string fileline, address;
-    while (getline(filereader, fileline))
-    {
-        if (fileline[0] == 'd' || fileline[0] == 'D')
-        {
-            address = fileline.substr(1);
-            cachesimulatorline(address, Cycles, index, disp, tag, numofaccess, hits, misses);
-        }
-        else if (fileline[0] == 'i' || fileline[0] == 'I')
-        {
-            address = fileline.substr(1);
-            Icachesimulatorline(address, Cycles, index, disp, tag, numofaccess, hits, misses);
-        }
-        // else
-        // {
-        //     address = fileline;
-        //     cachesimulatorline(address, Cycles, index, disp, tag, numofaccess, hits, misses);
-        // }
-    }
+ showMenu();
 }
